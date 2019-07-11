62d181a1a71d7050d502b1a853c1a112a021748a
elektronikyarasa <46841434+elektronikyarasa@users.noreply.github.com>
2019-07-11 22:43:31 +0300

    Update i2c-lcd.h

--- i2c-lcd.h
+++ i2c-lcd.h
@@ -1,14 +1,13 @@
 #include "stm32f1xx_hal.h"
 
 void lcd_init (void);   // initialize lcd
-
 void lcd_send_cmd (char cmd);  // send command to the lcd
-
 void lcd_send_data (char data);  // send data to the lcd
-
 void lcd_send_string (char *str);  // send string to the lcd
-
 void lcd_gotoxy(uint8_t x, uint8_t y);
+void lcd_home(void);
+void lcd_clear(void);
+void lcd_shiftDecrement(void);
 
 
 /** Display size ------------------------------------------------------------ */
